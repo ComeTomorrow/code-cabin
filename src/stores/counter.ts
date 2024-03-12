fa5e3cba7052c438916aeb69f267c499f16ab84b
ComeTomorrow <c1638212509@163.com>
2024-03-13 07:11:43 +0800

    初始框架搭建1.0

---
+++ src/stores/counter.ts
@@ -0,0 +1,16 @@
+import { defineStore } from "pinia"
+
+export const useCounterStore = defineStore('counter', {
+    state() {
+        return {
+            num: 1
+        }
+    },
+    actions: {
+        inc() {
+            this.num++
+        }
+    }
+})
+
+//  default useCounterStore;
---
+++ src/views/home.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useCounterStore } from "../stores/counter"
+
+const counter = useCounterStore()
+</script>
+
+<template>
+i am home
+  <div @click="counter.inc()">我是首页{{counter.num}}</div>
+</template>
